be75cc45a0027430fb61b4a927168c1518b0ea8b
Huayuan Song <huayuan.s@gmail.com>
2023-02-28 15:03:33 +0100

    Update __init__.py
    Removed deprecated code

--- stat_mod/__init__.py
+++ stat_mod/__init__.py
@@ -1,6 +1,3 @@
-from stat_mod.data_opt import *
-from stat_mod.datasets import *
 from stat_mod.definitions import *
 from stat_mod.figures import *
 from stat_mod.text import *
-from stat_mod.utils import *
